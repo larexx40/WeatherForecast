890c91b8edead3619a0c5734e42def27a865a9ca
root <root@localhost.localdomain>
2019-08-29 04:52:45 +0000

    navigation fragment setup

---
+++ app/src/main/java/com/example/weatherforecast/Ui/settings/SettingsFragment.kt
@@ -0,0 +1,6 @@
+package com.example.weatherforecast.Ui.settings
+
+import androidx.fragment.app.Fragment
+
+class SettingsFragment : Fragment(){
+}
---
+++ app/src/main/java/com/example/weatherforecast/Ui/weather/future/list/FutureListWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.weatherforecast.Ui.weather.future.list
+
+import androidx.lifecycle.ViewModel
+
+class FutureListWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
